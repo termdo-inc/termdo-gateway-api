2b732919dd63102133e10e15e1e4257282940595
EmrecanKaracayir <developer@karacayir.com>
2025-07-23 23:47:22 +0300

    Created: go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module termdo.com/termdo-gateway-api
+
+go 1.24.5
---
+++ source/app.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var height float64 = 1.75
+
+	fmt.Printf("Welcome to the Go application!, %.2f\n", height)
+}
